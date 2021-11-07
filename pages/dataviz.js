8dcefc1609b1b158ca0e496c905823698c49b38a
rgsk <guptarahul70322@gmail.com>
2021-11-07 13:38:36 +0530

    parsing csv

--- pages/dataviz.js
+++ pages/dataviz.js
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import * as d3 from "d3";
 const fetchCSV = async (url) => {
   const response = await fetch(url);
   const text = await response.text();
@@ -8,7 +9,21 @@ const Dataviz = () => {
   useEffect(() => {
     const csvUrl =
       "https://gist.githubusercontent.com/rgsk/62f6eef8a12601450938c6dd28c1e463/raw/cssNamedColors.csv";
-    fetchCSV(csvUrl).then((text) => console.log(text));
+    d3.csv(csvUrl).then((data) => {
+      const text = d3.csvFormat(data);
+      console.log(text.length + " bytes");
+      console.log(text.length / 1024 + " kb");
+      console.log(data.length + " rows");
+      console.log(data.columns.length + " columns");
+    });
+    // With fetch, async and await
+    // fetchCSV(csvUrl).then((text) => {
+    //   const data = d3.csvParse(text);
+    //   console.log(text.length + " bytes");
+    //   console.log(text.length / 1024 + " kb");
+    //   console.log(data.length + " rows");
+    //   console.log(data.columns.length + " columns");
+    // });
   }, []);
   return <div>data viz</div>;
 };
---
+++ pages/moveCircle.js
@@ -0,0 +1,37 @@
+import React, { useCallback, useState } from "react";
+const width = 480;
+const height = 500;
+const circleRadius = 30;
+const initalMousePosition = { x: width / 2, y: height / 2 };
+function MoveCircle() {
+  const [mousePosition, setMousePosition] = useState(initalMousePosition);
+  const handleMouseMove = useCallback(
+    (e) => {
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    },
+    [setMousePosition]
+  );
+  return (
+    <div>
+      <svg
+        style={{
+          border: "1px solid red",
+        }}
+        width={width}
+        height={height}
+        onMouseMove={handleMouseMove}
+      >
+        <circle
+          cx={mousePosition.x}
+          cy={mousePosition.y}
+          r={circleRadius}
+        ></circle>
+      </svg>
+    </div>
+  );
+}
+
+export default MoveCircle;
