8a1646bf2ea4ac8029b935721541ebc2e946c7b6
rgsk <guptarahul70322@gmail.com>
2021-11-14 09:52:46 +0530

    bar complete

--- components/Dataviz/Children/Bar.js
+++ components/Dataviz/Children/Bar.js
@@ -4,7 +4,7 @@ import * as d3 from "d3";
 const width = 960;
 const height = 500;
 const margin = {
-  left: 20,
+  left: 200,
   top: 20,
   right: 20,
   bottom: 20,
@@ -44,6 +44,33 @@ function Bar() {
     >
       <svg width={width} height={height}>
         <g transform={`translate(${margin.left}, ${margin.top})`}>
+          {xScale.ticks().map((tickValue, i) => (
+            <g key={i} transform={`translate(${xScale(tickValue)}, 0)`}>
+              <line stroke="black" y2={innerHeight} />
+              <text
+                y={innerHeight + 3}
+                dy=".71em"
+                style={{
+                  textAnchor: "middle",
+                }}
+              >
+                {tickValue}
+              </text>
+            </g>
+          ))}
+          {yScale.domain().map((tickValue, i) => (
+            <text
+              key={i}
+              x={-3}
+              dy=".32em"
+              y={yScale(tickValue) + yScale.bandwidth() / 2}
+              style={{
+                textAnchor: "end",
+              }}
+            >
+              {tickValue}
+            </text>
+          ))}
           {data.map((d, i) => (
             <rect
               key={i}
