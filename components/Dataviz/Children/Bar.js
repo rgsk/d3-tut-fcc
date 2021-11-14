9ad0370446f8053126017a689134c47405f6d5dd
rgsk <guptarahul70322@gmail.com>
2021-11-14 09:26:42 +0530

    margin

--- components/Dataviz/Children/Bar.js
+++ components/Dataviz/Children/Bar.js
@@ -3,6 +3,14 @@ import { useEffect, useState } from "react";
 import * as d3 from "d3";
 const width = 960;
 const height = 500;
+const margin = {
+  left: 20,
+  top: 20,
+  right: 20,
+  bottom: 20,
+};
+const innerHeight = height - margin.top - margin.bottom;
+const innerWidth = width - margin.left - margin.right;
 const unitedNationsUrl =
   "https://gist.githubusercontent.com/curran/0ac4077c7fc6390f5dd33bf5c06cb5ff/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv";
 
@@ -22,23 +30,30 @@ function Bar() {
   const yScale = d3
     .scaleBand()
     .domain(data.map((d) => d.Country))
-    .range([0, height]);
+    .range([0, innerHeight]);
   const xScale = d3
     .scaleLinear()
     .domain([0, d3.max(data, (d) => d.Population)])
-    .range([0, width]);
+    .range([0, innerWidth]);
   return (
-    <div>
+    <div
+      style={{
+        border: "1px solid red",
+        width: "min-content",
+      }}
+    >
       <svg width={width} height={height}>
-        {data.map((d, i) => (
-          <rect
-            key={i}
-            x={0}
-            y={yScale(d.Country)}
-            width={xScale(d.Population)}
-            height={yScale.bandwidth()}
-          />
-        ))}
+        <g transform={`translate(${margin.left}, ${margin.top})`}>
+          {data.map((d, i) => (
+            <rect
+              key={i}
+              x={0}
+              y={yScale(d.Country)}
+              width={xScale(d.Population)}
+              height={yScale.bandwidth()}
+            />
+          ))}
+        </g>
       </svg>
     </div>
   );
